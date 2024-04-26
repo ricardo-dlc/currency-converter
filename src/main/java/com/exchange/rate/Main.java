542c810cd29e4c26979448574fc21cfae59adca7
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-04-26 02:35:46 -0500

    Initial commit

---
+++ src/main/java/com/exchange/rate/Main.java
@@ -0,0 +1,9 @@
+package com.exchange.rate;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello World!");
+    }
+}
